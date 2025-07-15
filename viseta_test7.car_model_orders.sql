72b8ab5fa97727afe758c45f3775f374463f305d
Tala <94225670+TalaSadeq@users.noreply.github.com>
2025-07-15 09:46:14 +0300

    ay haga t commit

--- viseta_test7.car_model_orders.sql
+++ viseta_test7.car_model_orders.sql
@@ -1,7 +1,7 @@
 MODEL (
-  kind INCREMENTAL_BY_UNIQUE_KEY (unique_key id),
-  cron '0 2 * * *',
+  kind INCREMENTAL_BY_UNIQUE_KEY unique_key id lookback 2,
   owner 'customer_analytics',
+  cron '0 2 * * *',
   tags ('car', 'orders', 'model', 'analytics'),
   audits (
     assert_not_null(user_id, 'user_id must not be null'),
@@ -9,10 +9,9 @@ MODEL (
     assert_not_null(name_en, 'name_en must not be null'),
     assert_not_null(order_id, 'order_id must not be null')
   ),
-  description 'Calculates total orders and life time value per car model for Customer Service and Marketing teams',
-  lookback 2
+  description 'Generates total orders count and sum of grand_total (life_time_value) per car model (models.name_en) for Customer Service and Marketing teams'
 ) AS
-WITH cars_with_models AS (
+WITH cars_models AS (
   SELECT
     cars.id AS car_id,
     models.id AS model_id,
@@ -20,38 +19,33 @@ WITH cars_with_models AS (
   FROM cars
   JOIN models ON cars.id = models.id
 ),
-orders_with_users_cars AS (
+orders_users_cars AS (
   SELECT
     orders.id AS order_id,
     orders.created_at,
     orders.grand_total,
-    users.id AS user_id,
-    cars_with_models.car_id,
-    cars_with_models.model_id,
-    cars_with_models.name_en
+    orders.user_id,
+    orders.car_id,
+    cars_models.name_en
   FROM orders
-  JOIN users ON orders.id = users.id
-  JOIN cars_with_models ON users.id = cars_with_models.car_id
+  JOIN cars_models ON orders.car_id = cars_models.car_id
+  WHERE orders.user_id IS NOT NULL
+    AND orders.car_id IS NOT NULL
 ),
-aggregated_orders AS (
+final_agg AS (
   SELECT
     ROW_NUMBER() OVER (ORDER BY name_en) AS id,
     name_en,
     COUNT(order_id) AS count_of_orders,
-    COALESCE(SUM(grand_total), 0) AS life_time_value,
-    user_id,
-    car_id,
-    order_id
-  FROM orders_with_users_cars
-  WHERE user_id IS NOT NULL
-    AND car_id IS NOT NULL
-    AND name_en IS NOT NULL
+    COALESCE(SUM(grand_total), 0) AS life_time_value
+  FROM orders_users_cars
+  WHERE name_en IS NOT NULL
     AND order_id IS NOT NULL
-  GROUP BY name_en, user_id, car_id, order_id
+  GROUP BY name_en
 )
 SELECT
   id,
   name_en,
   count_of_orders,
   life_time_value
-FROM aggregated_orders;
+FROM final_agg;
